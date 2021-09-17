f813a8769740c6c18fcd2a3df8f04f6a35494e08
sangeethaNR <sangeethajadhav123@gmail.com>
2021-09-16 21:31:17 -0700

    handlebar and routes logic added

---
+++ models/Accessories.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Accessories extends Model {}
+
+Accessories.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+     user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+     
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Accessories',
+  }
+);
+
+module.exports = Accessories;

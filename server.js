d8782347c8811cad5719c5ddf2a499fdff7c970c
sangeethaNR <sangeethajadhav123@gmail.com>
2021-10-09 11:15:17 -0700

    compression added

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+var compression = require('compression')
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+app.use(compression({ filter: shouldCompress }))
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
